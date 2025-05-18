6fb7cff4806f5a16165fbbeb6b16c52f694b39ae
jeon-hakyung <jhakyung12@gmail.com>
2025-05-18 11:42:13 +0900

    :recycle:refactor: 7주차미션 에러 처리 코드 리팩토링 (#6)

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -2,27 +2,16 @@ import { createReviewService } from "../services/review.service.js";
 import { bodyToReview } from "../dtos/review.dto.js";
 import { listUserReviews } from "../services/review.service.js";
 
-export const createReview = async (req, res) => {
-  const storeId = parseInt(req.params.storeId);
-  const reviewData = bodyToReview(req.body, storeId);
-
-  try {
-    const result = await createReviewService(reviewData);
-    res.status(201).json({
-      isSuccess: true,
-      code: 201,
-      message: "리뷰 등록 성공",
-      result
-    });
-  } catch (err) {
-    res.status(400).json({
-      isSuccess: false,
-      code: 400,
-      message: err.message,
-      result: null
-    });
-  }
-};
+export const createReview = async (req, res, next) => {
+    const storeId = parseInt(req.params.storeId);
+  
+    try {
+      const result = await createReviewService(storeId, req.body);
+      return res.success(result);
+    } catch (err) {
+      next(err);
+    }
+  };
 
 export const handleListUserReviews = async (req, res) => {
     const userId = parseInt(req.params.userId);
